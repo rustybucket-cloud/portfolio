511d28192bce037bc56939d257767f1c289725a7
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2022-04-05 18:25:34 -0600

    improve project slide animation

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import './scss/styles.scss';
 import lg from "./images/jacob/lg.jpg"
 import github from "./images/icons/github-brands.svg"
 import linkedin from "./images/icons/linkedin-brands.svg"
-import ipTracker from "./images/projects/ip-tracker/ip-tracker.png"
-import { useState, useRef, useEffect } from "react"
+import { useState, useRef } from "react"
 import Project from './components/Project';
-import projects from "./projects.json"
-
-import { ReactComponent as Left } from "./images/icons/chevron-left.svg"
-import { ReactComponent as Right } from "./images/icons/chevron-right.svg"
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 
 const App = () => {
   const [ active, setActive ] = useState('about')
-  const [ project, setProject ] = useState(0)
-  const [ animation, setAnimation ] = useState(false)
-  
-  const projectPageRef = useRef(null)
 
   const aboutRef = useRef(null)
   const resumeRef = useRef(null)
@@ -37,64 +29,6 @@ const App = () => {
     else if (location === "projects") projectsRef.current.scrollIntoView()
     else if (location === "contact") contactRef.current.scrollIntoView()
   }
-
-  const changeProject = (direction) => {
-    if (animation) return
-      setTimeout(() => {
-      if (direction === "next") {
-        if (project === projects.length - 1) setProject(0)
-        else setProject(project => project += 1)
-      }
-      else if (direction === "back") {
-        if (project === 0) setProject(projects.length - 1)
-        else setProject(project => project -= 1)
-      }
-    }, 500)
-    runAnimation(direction)
-  }
-
-  const runAnimation = (direction) => {
-    setAnimation(true)
-    projectPageRef.current.classList.add(direction)
-    setTimeout(() => {
-      projectPageRef.current.classList.add("enter")
-      setTimeout(() => {
-        projectPageRef.current.classList.remove(direction)
-        projectPageRef.current.classList.remove("enter")
-        setAnimation(null)
-      }, 700)
-    }, 700)
-  }
-
-  useEffect(() => {
-    if (animation) {
-      setTimeout(() => setAnimation(false), 1500)
-    }
-  }, [animation])
-
-  /* useEffect(() => {
-    if (animation === 'next') {
-      projectPageRef.current.classList.add("next")
-      setTimeout(() => {
-        projectPageRef.current.classList.add("enter")
-        setTimeout(() => {
-          projectPageRef.current.classList.remove("next")
-          projectPageRef.current.classList.remove("enter")
-          setAnimation(null)
-        }, 750)
-      }, 750)
-    } else if (animation === 'back') {
-      projectPageRef.current.classList.add("back")
-      setTimeout(() => {
-        projectPageRef.current.classList.add("enter")
-        setTimeout(() => {
-          projectPageRef.current.classList.remove("back")
-          projectPageRef.current.classList.remove("enter")
-          setAnimation(null)
-        }, 750)
-      }, 750)
-    }
-  }, [animation]) */
   
   return (
     <div className="App" >
@@ -106,83 +40,74 @@ const App = () => {
           <li className={active === "contact" ?  "active" : ""}><a href="#contact-me">Contact</a></li>
         </ul>
       </nav>
+    <Router>
+      <Route path="/">
+        <svg className="nav__svg" width="20px">
+          <circle onClick={() => handleClick('about')} id="scroll-about" cx="8px" cy="8px" r={active === "about" ?  "8px" : "4px"} fill="white"/>
+          <circle onClick={() => handleClick('resume')} cx="8px" cy="32px" r={active === "resume" ?  "8px" : "4px"} fill="white"/>
+          <circle onClick={() => handleClick('projects')} cx="8px" cy="56px" r={active === "projects" ?  "8px" : "4px"} fill="white"/>
+          <circle onClick={() => handleClick('contact')} cx="8px" cy="80px" r={active === "contact" ?  "8px" : "4px"} fill="white"/>
+        </svg>
 
-     <svg className="nav__svg" width="20px">
-       <circle onClick={() => handleClick('about')} id="scroll-about" cx="8px" cy="8px" r={active === "about" ?  "8px" : "4px"} fill="white"/>
-       <circle onClick={() => handleClick('resume')} cx="8px" cy="32px" r={active === "resume" ?  "8px" : "4px"} fill="white"/>
-       <circle onClick={() => handleClick('projects')} cx="8px" cy="56px" r={active === "projects" ?  "8px" : "4px"} fill="white"/>
-       <circle onClick={() => handleClick('contact')} cx="8px" cy="80px" r={active === "contact" ?  "8px" : "4px"} fill="white"/>
-     </svg>
-
-      <section ref={aboutRef} aria-label="About Me" id="about-me" className="about-me">
-        <h1>Hi! I'm Jacob Patton</h1>
-        <p>I'm a Web developer located in Provo, Ut</p>
-        <picture>
-          <img src={lg} alt="Me with a tree in the background"/>
-        </picture>
-        <div className="icons">
-          <img src={github} alt=""/>
-          <img src={linkedin} alt=""/>
-        </div>
-      </section>
-
-      <section ref={resumeRef} id="resume" className="resume">
-        <div className="section-content">
-          <h2>Resume</h2>
-
-          <h3>Education</h3>
-          <div className="education resume__section">
-            <h4>Utah Valley University</h4>
-            <p className="timeFrame">2019-2021</p>
-            <p>Associates of Science</p>
-          </div>
-
-          <h3>Work Experience</h3>
-          <div className="education resume__section">
-            <div className="resume__section__title">
-              <h4>Target</h4>
-              <p>General Merchandise Expert</p>
+          <section ref={aboutRef} aria-label="About Me" id="about-me" className="about-me">
+            <h1>Hi! I'm Jacob Patton</h1>
+            <p>I'm a Web developer located in Provo, Ut</p>
+            <picture>
+              <img src={lg} alt="Me with a tree in the background"/>
+            </picture>
+            <div className="icons">
+              <img src={github} alt=""/>
+              <img src={linkedin} alt=""/>
             </div>
-            <p  className="timeFrame">2020-Current</p>
-            <ul>
-              <li>Work in a team setting to ensure that time sensitive tasks are accomplished</li>
-              <li>Solve problems for customers</li>
-            </ul>
-          </div>
+          </section>
 
-          <h3>Skills</h3>
-          <div className="resume__section">
-            <ul>
-              <li>HTML, CSS, Javascript</li>
-              <li>Express js</li>
-              <li>React</li>
-              <li>jQuery</li>
-              <li>SCSS</li>
-              <li>Figma/Adobe XD</li>
-              <li>Fluent Spanish speaker</li>
-            </ul>
-          </div>
-        </div>
-      </section>
+          <section ref={resumeRef} id="resume" className="resume">
+            <div className="section-content">
+              <h2>Resume</h2>
 
-      <section ref={projectsRef} id="projects" className="projects">
-        <div className="section-content">
-          <h2>Projects</h2>
-          <Left className="left" onClick={() => {
-            changeProject("back")
-            runAnimation("back")
-          }} />
-          <div ref={projectPageRef} className="project">
-            { projects && <Project name={projects[project].name} img={ipTracker} used={projects[project].used} challenges={projects[project].challenges}/> }
-          </div>
-          <Right className="right" onClick={() => {
-            changeProject("next")
-            runAnimation("next")
-          }} />
-        </div>
-      </section>
-    
-      <section ref={contactRef} id="contact-me" className="contact-me">
+              <h3>Education</h3>
+              <div className="education resume__section">
+                <h4>Utah Valley University</h4>
+                <p className="timeFrame">2019-2021</p>
+                <p>Associates of Science</p>
+              </div>
+
+              <h3>Work Experience</h3>
+              <div className="education resume__section">
+                <div className="resume__section__title">
+                  <h4>Target</h4>
+                  <p>General Merchandise Expert</p>
+                </div>
+                <p  className="timeFrame">2020-Current</p>
+                <ul>
+                  <li>Work in a team setting to ensure that time sensitive tasks are accomplished</li>
+                  <li>Solve problems for customers</li>
+                </ul>
+              </div>
+
+              <h3>Skills</h3>
+              <div className="resume__section">
+                <ul>
+                  <li>HTML, CSS, Javascript</li>
+                  <li>Express js</li>
+                  <li>React</li>
+                  <li>jQuery</li>
+                  <li>SCSS</li>
+                  <li>Figma/Adobe XD</li>
+                  <li>Fluent Spanish speaker</li>
+                </ul>
+              </div>
+            </div>
+          </section>
+
+          <section ref={projectsRef} id="projects" className="projects">
+            <div className="section-content">
+              <h2>Projects</h2>
+              <Project />
+            </div>
+          </section>
+        
+          <section ref={contactRef} id="contact-me" className="contact-me">
         <div className="section-content">
           <h2>Contact Me</h2>
           <form>
@@ -199,6 +124,8 @@ const App = () => {
           </form>
         </div>
       </section>
+      </Route>
+    </Router>
     </div>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,110 @@
+import { ReactComponent as Left } from "../images/icons/chevron-left.svg"
+import { ReactComponent as Right } from "../images/icons/chevron-right.svg"
+import projects from "../projects.json"
+import ipTracker from "../images/projects/ip-tracker/ip-tracker.png"
+
+import { useRef, useState, useEffect } from "react"
+
 export default function Project({name, img, used, challenges}) {
-    return (
-        <>
-        <h3>{name}</h3>
+  const [ project, setProject ] = useState(0)
+  const [ animation, setAnimation ] = useState(false)
+  const [ projectInfo, setProjectInfo ] = useState(null)
+
+  useEffect(() => {
+    setProjectInfo({
+      name: projects[project].name, 
+      img: ipTracker,
+      used: projects[project].used,
+      challenges: projects[project].challenges,
+      live: projects[project].live,
+      code: projects[project].code,
+      rel: projects[project].name.replace(" ", "").toLowerCase()
+    })
+  }, [project])
+
+  useEffect(() => {
+    if (animation) {
+      setTimeout(() => setAnimation(false), 1500)
+    }
+  }, [animation])
+  
+
+  const projectRef = useRef(null)
+
+  const changeProject = (direction) => {
+    if (animation) return
+    setAnimation(true)
+    console.log(animation)
+    setTimeout(() => {
+      if (direction === "next") {
+        if (project === projects.length - 1) setProject(0)
+        else setProject(project => project += 1)
+      }
+      else if (direction === "back") {
+        if (project === 0) setProject(projects.length - 1)
+        else setProject(project => project -= 1)
+      }
+    }, 500)
+    runAnimation(direction)
+  }
+
+  const runAnimation = (direction) => {
+    projectRef.current.classList.add(direction)
+    setTimeout(() => {
+      setTimeout(() => {
+        projectRef.current.classList.remove(direction)
+      }, 700)
+    }, 700)
+  }
+
+    return projectInfo ? (
+      <div ref={projectRef} className="project">
+        <h3>{projectInfo.name}</h3>
           <figure>
-            <img src={img} alt="a screenshot of a project"/>
+            <img src={projectInfo.img} alt="a screenshot of a project"/>
           </figure>
+          <div className="projects__btns">
+            <a href={projectInfo.live} target="_blank">Live Site</a>
+            <a href={projectInfo.code} target="_blank">Code</a>
+            <a href={`../${projectInfo.rel}`}>About this Project</a>
+          </div>
           <div className="projects__used">
-            <div>
+           {/*  <div>
               <h4>I Used</h4>
               <ul>
-                {used.map( (item, i) => <li key={i}>{item}</li>)}
+                {projectInfo.used.map( (item, i) => <li key={i}>{item}</li>)}
               </ul>
             </div>
             
             <div>
               <h4>Challenges</h4>
               <ul>
-                {challenges.map( (challenge, i) => <li key={i}>{challenge}</li>)}
+                {projectInfo.challenges.map( (challenge, i) => <li key={i}>{challenge}</li>)}
               </ul>
-            </div>
+            </div> */}
           </div>
-        </>
-    )
+          <button className="left change-project"
+            onClick={() => {
+              changeProject("back")
+              runAnimation("back")
+            }}
+            style={{
+              display: animation ? "none" : "block"
+            }}
+          >
+            <Left />
+          </button>
+          <button  className="right change-project" 
+            onClick={() => {
+                changeProject("next")
+                runAnimation("next")
+            }}
+            style={{
+              display: animation ? "none" : "block"
+            }}
+          >
+            <Right/>
+          </button>
+        </div>
+    ) : null
 }
